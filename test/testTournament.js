82dfaabb07b1612aafec9a582d2f54e5e051afdd
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-01-25 02:08:23 -0500

    Added some simple getters and setters for the Tournament class

--- test/testTournament.js
+++ test/testTournament.js
@@ -19,17 +19,49 @@ const BAD_TOURNAMENT_NAME = 'badnamedotexe';
 
 describe('Smash GG Tournament', function(){
 
-    afterEach(function(done){
-        Cache.flush();
-    });
-
-    it('should correctly load tournament data', async function(done){
+    it('should correctly load tournament data', async function(){
         tournament1 = new Tournament(TOURNAMENT_NAME1);
         tournament2 = new Tournament(TOURNAMENT_NAME2);
 
         await tournament1.load();
         await tournament2.load();
 
+        return true;
+    });
+
+    it('should return the correct tournament id', function(done){
+        let id1 = tournament1.getId();
+        let id2 = tournament2.getId();
+
+        //TODO compare
+
         done();
-    })
+    });
+
+    it('should return the correct tournament name', function(done){
+        let name1 = tournament1.getName();
+        let name2 = tournament2.getName();
+
+        //TODO compare
+
+        done();
+    });
+
+    it('should return the correct starting time', function(done){
+        let startTime1 = tournament1.getStartTime();
+        let startTime2 = tournament2.getStartTime();
+
+        //TODO compare
+
+        done();
+    });
+
+    it('should return the correct starting time', function(done){
+        let endTime1 = tournament1.getEndTime();
+        let endTime2 = tournament2.getEndTime();
+
+        //TODO compare
+
+        done();
+    });
 });

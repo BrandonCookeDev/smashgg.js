2178fbe6cd481cae67457490d87413dc8f88631e
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-01-25 12:28:31 -0500

    Added base phase group object and test suite

---
+++ test/testPhaseGroup.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let _ = require('lodash');
+
+let PhaseGroup = require('../lib/PhaseGroup');
+let Cache = require('../lib/util/Cache');
+
+let chai = require('chai');
+let cap = require('chai-as-promised');
+chai.use(cap);
+
+let expect = chai.expect;
+let assert = chai.assert;
+
+let phaseGroup1 = {};
+let phaseGroup2 = {};
+let phaseGroup3 = {};
+let phaseGroup4 = {};
+
+const ID1 = 0;
+const ID2 = 0;
+const ID3 = 301994;
+
+let expected = _.extend(
+
+);
+
+describe('Smash GG Phase Group', function(){
+
+    it('should correctly load Phase Group data', function(done){
+        phaseGroup3 = new PhaseGroup(ID3);
+        phaseGroup3.on('ready', done);
+    })
+
+});

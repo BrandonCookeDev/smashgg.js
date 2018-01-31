6971b7a76ecf4477c345f17d6a68f5ff6dce28e3
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-01-30 22:36:24 -0500

    simplified npm package structure by just linking objects from the root index

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-modules.exports = require('./lib');
+exports.Set         = require('./lib/Set');
+exports.Event       = require('./lib/Event');
+exports.Phase       = require('./lib/Phase');
+exports.Player      = require('./lib/Player');
+exports.Tournament  = require('./lib/Tournament');
+exports.PhaseGroup  = require('./lib/PhaseGroup');

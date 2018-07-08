846eacfbfcfd193b4c005e707f30c595c5498168
Brandon Cooke <brandoncookedev@gmail.com>
2018-07-08 03:23:34 -0400

    Added eslint for formatting. Fixed code to format standards

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-exports.Set         = require('./lib/Set');
-exports.Event       = require('./lib/Event');
-exports.Phase       = require('./lib/Phase');
-exports.Player      = require('./lib/Player');
+exports.Set		 	= require('./lib/Set');
+exports.Event	   	= require('./lib/Event');
+exports.Phase	   	= require('./lib/Phase');
+exports.Player	  	= require('./lib/Player');
 exports.Tournament  = require('./lib/Tournament');
 exports.PhaseGroup  = require('./lib/PhaseGroup');
--- test/testPhase.js
+++ test/testPhase.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 'use strict';
 
 let _ = require('lodash');
@@ -14,7 +15,7 @@ let expect = chai.expect;
 let assert = chai.assert;
 
 let expected = _.extend(
-    require('./data/testSets')
+	require('./data/testSets')
 );
 
 let ID1 = 111483;
@@ -24,85 +25,85 @@ let ID3 = 100046;
 let phase1, phase2, phase3;
 
 function loadPhase(id, options){
-    return new Promise(function(resolve, reject){
-        let P = new Phase(id, options);
-        P.on('ready', function(){
-            resolve(P);
-        })
-    })
+	return new Promise(function(resolve, reject){
+		let P = new Phase(id, options);
+		P.on('ready', function(){
+			resolve(P);
+		})
+	})
 }
 
 describe('Smash GG Phase', function(){
 
-    before(function(){
-        Cache.flush();
-    });
+	before(function(){
+		Cache.flush();
+	});
 
-    it('should correctly load the Phase', async function(){
-        this.timeout(10000);
+	it('should correctly load the Phase', async function(){
+		this.timeout(10000);
 
-        phase1 = await loadPhase(ID1, {rawEncoding: 'utf8'});
-        phase2 = await loadPhase(ID2);
-        phase3 = await loadPhase(ID3, {rawEncoding: 'base64'});
-        return true;
-    });
+		phase1 = await loadPhase(ID1, {rawEncoding: 'utf8'});
+		phase2 = await loadPhase(ID2);
+		phase3 = await loadPhase(ID3, {rawEncoding: 'base64'});
+		return true;
+	});
 
-    it('should implement the convenience methods correctly', async function(){
-        this.timeout(10000);
+	it('should implement the convenience methods correctly', async function(){
+		this.timeout(10000);
 
-        let cPhase1 = await Phase.getPhase(ID1);
-        let cPhase2 = await Phase.getPhase(ID2);
-        let cPhase3 = await Phase.getPhase(ID3);
+		let cPhase1 = await Phase.getPhase(ID1);
+		let cPhase2 = await Phase.getPhase(ID2);
+		let cPhase3 = await Phase.getPhase(ID3);
 
-        expect(cPhase1.data).to.deep.equal(phase1.data);
-        expect(cPhase2.data).to.deep.equal(phase2.data);
-        expect(cPhase3.data).to.deep.equal(phase3.data);
+		expect(cPhase1.data).to.deep.equal(phase1.data);
+		expect(cPhase2.data).to.deep.equal(phase2.data);
+		expect(cPhase3.data).to.deep.equal(phase3.data);
 
-        return true;
-    })
+		return true;
+	})
 
-    it('should get the name of the Phase', function(done){
-        expect(phase1.getName()).to.be.equal('Pools');
-        expect(phase2.getName()).to.be.equal('Pools');
-        expect(phase3.getName()).to.be.equal('Bracket Pools');
-        done();
-    });
+	it('should get the name of the Phase', function(done){
+		expect(phase1.getName()).to.be.equal('Pools');
+		expect(phase2.getName()).to.be.equal('Pools');
+		expect(phase3.getName()).to.be.equal('Bracket Pools');
+		done();
+	});
 
-    it('should get the event id', function(done){
-        expect(phase1.getEventId()).to.be.equal(25545);
-        expect(phase2.getEventId()).to.be.equal(11787);
-        expect(phase3.getEventId()).to.be.equal(23596);
-        done();
-    });
+	it('should get the event id', function(done){
+		expect(phase1.getEventId()).to.be.equal(25545);
+		expect(phase2.getEventId()).to.be.equal(11787);
+		expect(phase3.getEventId()).to.be.equal(23596);
+		done();
+	});
 
-    it('should correctly get all phase groups', async function(){
-        this.timeout(30000);
+	it('should correctly get all phase groups', async function(){
+		this.timeout(30000);
 
-        let phaseGroups1 = await phase1.getPhaseGroups();
-        let phaseGroups2 = await phase2.getPhaseGroups();
-        let phaseGroups3 = await phase3.getPhaseGroups();
+		let phaseGroups1 = await phase1.getPhaseGroups();
+		let phaseGroups2 = await phase2.getPhaseGroups();
+		let phaseGroups3 = await phase3.getPhaseGroups();
 
-        expect(phaseGroups1.length).to.be.equal(16);
-        expect(phaseGroups2.length).to.be.equal(32);
-        expect(phaseGroups3.length).to.be.equal(16);
+		expect(phaseGroups1.length).to.be.equal(16);
+		expect(phaseGroups2.length).to.be.equal(32);
+		expect(phaseGroups3.length).to.be.equal(16);
 
-        var hasDuplicates = function(a) {
-            return _.uniq(a).length !== a.length;
-        };
-        expect(hasDuplicates(phaseGroups1)).to.be.false;
-        expect(hasDuplicates(phaseGroups2)).to.be.false;
-        expect(hasDuplicates(phaseGroups3)).to.be.false;
+		var hasDuplicates = function(a) {
+			return _.uniq(a).length !== a.length;
+		};
+		expect(hasDuplicates(phaseGroups1)).to.be.false;
+		expect(hasDuplicates(phaseGroups2)).to.be.false;
+		expect(hasDuplicates(phaseGroups3)).to.be.false;
 
-        phaseGroups1.forEach(set => {
-            expect(set).to.be.an.instanceof(PhaseGroup);
-        });
-        phaseGroups2.forEach(set => {
-            expect(set).to.be.an.instanceof(PhaseGroup);
-        });
-        phaseGroups3.forEach(set => {
-            expect(set).to.be.an.instanceof(PhaseGroup);
-        });
+		phaseGroups1.forEach(set => {
+			expect(set).to.be.an.instanceof(PhaseGroup);
+		});
+		phaseGroups2.forEach(set => {
+			expect(set).to.be.an.instanceof(PhaseGroup);
+		});
+		phaseGroups3.forEach(set => {
+			expect(set).to.be.an.instanceof(PhaseGroup);
+		});
 
-        return true;
-    });
+		return true;
+	});
 });

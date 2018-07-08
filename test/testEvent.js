fa56c1334a9910efb9dd57a1f897390e5b8f0ec8
Brandon Cooke <brandoncookedev@gmail.com>
2018-07-08 03:23:34 -0400

    Added eslint for formatting. Fixed code to format standards

--- test/testEvent.js
+++ test/testEvent.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 'use strict';
 
 Promise = require('bluebird');
@@ -40,168 +41,168 @@ let expected = _.extend(
 );
 
 function loadEvent(eventName, tournamentName, options){
-    return new Promise(function(resolve, reject){
-        let event = new Event(eventName, tournamentName, options);
-        event.on('ready', function(){
-            resolve(event);
-        })
-    })
+	return new Promise(function(resolve, reject){
+		let event = new Event(eventName, tournamentName, options);
+		event.on('ready', function(){
+			resolve(event);
+		})
+	})
 }
 
 function loadEventViaId(id, options){
-    return new Promise(function(resolve, reject){
-        if(isNaN(id))
-            return reject('ID must be an integer');
-        let event = new Event(id, null, options);
-        event.on('ready', function(){
-            resolve(event);
-        })
-        event.on('error', function(err){
-            console.error(err);
-        })
-    })
+	return new Promise(function(resolve, reject){
+		if(isNaN(id))
+			return reject('ID must be an integer');
+		let event = new Event(id, null, options);
+		event.on('ready', function(){
+			resolve(event);
+		})
+		event.on('error', function(err){
+			console.error(err);
+		})
+	})
 }
 
 describe('Smash GG Event', function(){
 
-    before(function(){
-        Cache.flush();
-    });
+	before(function(){
+		Cache.flush();
+	});
 
-    it('should correctly load the data', async function(){
-        this.timeout(15000);
+	it('should correctly load the data', async function(){
+		this.timeout(15000);
 
-        event1 = await loadEvent(EVENT_NAME1, TOURNAMENT_NAME1, { rawEncoding: 'utf8'});
-        event2 = await loadEvent(EVENT_NAME1, TOURNAMENT_NAME2,);
-        event3 = await loadEventViaId(EVENT_ID_1, {rawEncoding: 'base64'});
+		event1 = await loadEvent(EVENT_NAME1, TOURNAMENT_NAME1, { rawEncoding: 'utf8'});
+		event2 = await loadEvent(EVENT_NAME1, TOURNAMENT_NAME2,);
+		event3 = await loadEventViaId(EVENT_ID_1, {rawEncoding: 'base64'});
 
-        return true;
-    });
+		return true;
+	});
 
-    it('should correctly implement convenience methods', async function(){
-        this.timeout(15000);
+	it('should correctly implement convenience methods', async function(){
+		this.timeout(15000);
 
-        let cEvent1 = await Event.getEvent(EVENT_NAME1, TOURNAMENT_NAME1, { rawEncoding: 'utf8'});
-        let cEvent2 = await Event.getEvent(EVENT_NAME1, TOURNAMENT_NAME2);
-        let cEvent3 = await Event.getEventById(EVENT_ID_1, {rawEncoding: 'base64'});
+		let cEvent1 = await Event.getEvent(EVENT_NAME1, TOURNAMENT_NAME1, { rawEncoding: 'utf8'});
+		let cEvent2 = await Event.getEvent(EVENT_NAME1, TOURNAMENT_NAME2);
+		let cEvent3 = await Event.getEventById(EVENT_ID_1, {rawEncoding: 'base64'});
 
-        expect(cEvent1.data).to.deep.equal(event1.data);
-        expect(cEvent2.data).to.deep.equal(event2.data);
-        expect(cEvent3.data).to.deep.equal(event3.data);
+		expect(cEvent1.data).to.deep.equal(event1.data);
+		expect(cEvent2.data).to.deep.equal(event2.data);
+		expect(cEvent3.data).to.deep.equal(event3.data);
 
-        return true;
-    })
+		return true;
+	})
 
-    it('should correctly get the event name', function(done){
-        let name1 = event1.getName();
-        let name3 = event3.getName();
-        expect(name1).to.be.equal('Melee Singles');
-        expect(name3).to.be.equal('Rocket League 3v3');
-        done();
-    });
+	it('should correctly get the event name', function(done){
+		let name1 = event1.getName();
+		let name3 = event3.getName();
+		expect(name1).to.be.equal('Melee Singles');
+		expect(name3).to.be.equal('Rocket League 3v3');
+		done();
+	});
 
-    it('should correctly get the event slug', function(done){
-        let slug = event1.getSlug();
-        expect(slug).to.be.equal('tournament/function-1-recursion-regional/event/melee-singles');
-        done();
-    });
+	it('should correctly get the event slug', function(done){
+		let slug = event1.getSlug();
+		expect(slug).to.be.equal('tournament/function-1-recursion-regional/event/melee-singles');
+		done();
+	});
 
-    it('should correctly get the event start time', function(done){
-        let startTime1 = event1.getStartTime();
-        let expected = moment('04-01-2017 11:00:00').toDate();
-        expect(startTime1.getTime()).to.be.equal(expected.getTime());
-        done();
-    });
+	it('should correctly get the event start time', function(done){
+		let startTime1 = event1.getStartTime();
+		let expected = moment('04-01-2017 11:00:00').toDate();
+		expect(startTime1.getTime()).to.be.equal(expected.getTime());
+		done();
+	});
 
-    it('should correctly get the event start time string', function(done){
-        let startTime1 = event1.getStartTimeString();
+	it('should correctly get the event start time string', function(done){
+		let startTime1 = event1.getStartTimeString();
 
-        try {
-            expect(startTime1).to.be.equal('04-01-2017 11:00:00 EST');
-        }
-        catch(e){
-            expect(startTime1).to.be.equal('04-01-2017 11:00:00 EDT');
-        }
-        done();
-    });
+		try {
+			expect(startTime1).to.be.equal('04-01-2017 11:00:00 EST');
+		}
+		catch(e){
+			expect(startTime1).to.be.equal('04-01-2017 11:00:00 EDT');
+		}
+		done();
+	});
 
-    it('should correctly get the event end time', function(done){
-        let endTime1 = event1.getEndTime();
-        let expected = moment('04-01-2017 12:00:00').toDate();
-        expect(endTime1.getTime()).to.be.equal(expected.getTime());
-        done();
-    });
+	it('should correctly get the event end time', function(done){
+		let endTime1 = event1.getEndTime();
+		let expected = moment('04-01-2017 12:00:00').toDate();
+		expect(endTime1.getTime()).to.be.equal(expected.getTime());
+		done();
+	});
 
-    it('should correctly get the event end time string', function(done){
-        let endTime1 = event1.getEndTimeString();
+	it('should correctly get the event end time string', function(done){
+		let endTime1 = event1.getEndTimeString();
 
-        try {
-            expect(endTime1).to.be.equal('04-01-2017 12:00:00 EST');
-        }
-        catch(e){
-            expect(endTime1).to.be.equal('04-01-2017 12:00:00 EDT');
-        }
-        done();
-    });
+		try {
+			expect(endTime1).to.be.equal('04-01-2017 12:00:00 EST');
+		}
+		catch(e){
+			expect(endTime1).to.be.equal('04-01-2017 12:00:00 EDT');
+		}
+		done();
+	});
 
-    it('should correctly get the tournament slugs', function(){
-        let slug1 = event1.getTournamentSlug();
-        let slug2 = event2.getTournamentSlug();
-        let slug3 = event3.getTournamentSlug();
+	it('should correctly get the tournament slugs', function(){
+		let slug1 = event1.getTournamentSlug();
+		let slug2 = event2.getTournamentSlug();
+		let slug3 = event3.getTournamentSlug();
 
-        expect(slug1).to.be.equal('function-1-recursion-regional');
-        expect(slug2).to.be.equal('ceo-2016');
-        expect(slug3).to.be.equal('pulsar-premier-league');
-    })
+		expect(slug1).to.be.equal('function-1-recursion-regional');
+		expect(slug2).to.be.equal('ceo-2016');
+		expect(slug3).to.be.equal('pulsar-premier-league');
+	})
 
-    it('should correctly get the phases', async function(){
-        this.timeout(15000);
+	it('should correctly get the phases', async function(){
+		this.timeout(15000);
 
-        let phases1 = await event1.getEventPhases();
-        let phases2 = await event2.getEventPhases();
+		let phases1 = await event1.getEventPhases();
+		let phases2 = await event2.getEventPhases();
 
-        expect(phases1.length).to.be.equal(4);
-        expect(phases2.length).to.be.equal(2);
+		expect(phases1.length).to.be.equal(4);
+		expect(phases2.length).to.be.equal(2);
 
-        var hasDuplicates = function(a) {
-            return _.uniq(a).length !== a.length;
-        };
-        expect(hasDuplicates(phases1)).to.be.false;
-        expect(hasDuplicates(phases2)).to.be.false;
+		var hasDuplicates = function(a) {
+			return _.uniq(a).length !== a.length;
+		};
+		expect(hasDuplicates(phases1)).to.be.false;
+		expect(hasDuplicates(phases2)).to.be.false;
 
-        phases1.forEach(phase => {
-            expect(phase).to.be.instanceof(Phase);
-        });
+		phases1.forEach(phase => {
+			expect(phase).to.be.instanceof(Phase);
+		});
 
-        phases2.forEach(phase => {
-            expect(phase).to.be.instanceof(Phase);
-        });
+		phases2.forEach(phase => {
+			expect(phase).to.be.instanceof(Phase);
+		});
 
-        return true;
-    });
+		return true;
+	});
 
-    it('should correctly get the phase groups', async function(){
-        this.timeout(25000);
+	it('should correctly get the phase groups', async function(){
+		this.timeout(25000);
 
-        let groups1 = await event1.getEventPhaseGroups();
-        let groups2 = await event2.getEventPhaseGroups();
+		let groups1 = await event1.getEventPhaseGroups();
+		let groups2 = await event2.getEventPhaseGroups();
 
-        expect(groups1.length).to.be.equal(22);
-        expect(groups2.length).to.be.equal(33);
+		expect(groups1.length).to.be.equal(22);
+		expect(groups2.length).to.be.equal(33);
 
-        var hasDuplicates = function(a) {
-            return _.uniq(a).length !== a.length;
-        };
-        expect(hasDuplicates(groups1)).to.be.false;
-        expect(hasDuplicates(groups2)).to.be.false;
+		var hasDuplicates = function(a) {
+			return _.uniq(a).length !== a.length;
+		};
+		expect(hasDuplicates(groups1)).to.be.false;
+		expect(hasDuplicates(groups2)).to.be.false;
 
-        groups1.forEach(group => {
-            expect(group).to.be.instanceof(PhaseGroup);
-        });
-        groups2.forEach(group => {
-            expect(group).to.be.instanceof(PhaseGroup);
-        });
+		groups1.forEach(group => {
+			expect(group).to.be.instanceof(PhaseGroup);
+		});
+		groups2.forEach(group => {
+			expect(group).to.be.instanceof(PhaseGroup);
+		});
 
-        return true;
-    })
+		return true;
+	})
 });
--- test/testPhaseGroup.js
+++ test/testPhaseGroup.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 'use strict';
 
 Promise = require('bluebird');
@@ -28,55 +29,55 @@ let expected = _.extend(
 
 
 function loadPhaseGroup(id, options){
-    return new Promise(function(resolve, reject){
-        let PG = new PhaseGroup(id, options);
-        PG.on('ready', function(){
-            resolve(PG);
-        })
-    })
+	return new Promise(function(resolve, reject){
+		let PG = new PhaseGroup(id, options);
+		PG.on('ready', function(){
+			resolve(PG);
+		})
+	})
 }
 
 
 describe('Smash GG Phase Group', function(){
 
-    before(function(){
-        Cache.flush();
-    });
+	before(function(){
+		Cache.flush();
+	});
 
-    it('should correctly load Phase Group data', function(done){
-        this.timeout(5000);
+	it('should correctly load Phase Group data', function(done){
+		this.timeout(5000);
 
-        phaseGroup3 = new PhaseGroup(ID3, {rawEncoding: 'base64'});
-        phaseGroup3.on('ready', done);
-    });
+		phaseGroup3 = new PhaseGroup(ID3, {rawEncoding: 'base64'});
+		phaseGroup3.on('ready', done);
+	});
 
-    it('should implement the convenience methods correctly', async function(){
-        this.timeout(5000);
-        let cPhaseGroup3 = await PhaseGroup.getPhaseGroup(ID3);
-        expect(cPhaseGroup3.data).to.deep.equal(phaseGroup3.data);
-        return true;
-    })
+	it('should implement the convenience methods correctly', async function(){
+		this.timeout(5000);
+		let cPhaseGroup3 = await PhaseGroup.getPhaseGroup(ID3);
+		expect(cPhaseGroup3.data).to.deep.equal(phaseGroup3.data);
+		return true;
+	})
 
-    it('should correctly return the phase id', function(done){
-        let phaseId1 = phaseGroup3.getPhaseId();
-        expect(phaseId1).to.be.equal(100046);
-        done();
-    });
+	it('should correctly return the phase id', function(done){
+		let phaseId1 = phaseGroup3.getPhaseId();
+		expect(phaseId1).to.be.equal(100046);
+		done();
+	});
 
-    it('should get all entrants', async function(){
-        this.timeout(5000);
+	it('should get all entrants', async function(){
+		this.timeout(5000);
 
-        let players = await phaseGroup3.getPlayers();
-        expect(players.length).to.be.equal(15);
-        return true;
-    });
+		let players = await phaseGroup3.getPlayers();
+		expect(players.length).to.be.equal(15);
+		return true;
+	});
 
-    it('should get all sets', async function(){
-        this.timeout(5000);
+	it('should get all sets', async function(){
+		this.timeout(5000);
 
-        let sets = await phaseGroup3.getSets();
-        expect(sets.length).to.be.equal(26);
-        return true;
-    });
+		let sets = await phaseGroup3.getSets();
+		expect(sets.length).to.be.equal(26);
+		return true;
+	});
 
 });

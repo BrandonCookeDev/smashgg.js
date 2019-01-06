2d53e80bcd9a2854480e5491994ccbfd2a7c29d5
Brandon Cooke <BCooke91@Gmail.com>
2019-01-06 00:15:12 -0500

    working in entity fetcher

--- ts/lib/util/EntityFetcher.ts
+++ ts/lib/util/EntityFetcher.ts
@@ -6,15 +6,25 @@ import * as Common from './Common'
 import Entity = Common.Entity
 import Data = Common.Data
 
+
+import { ITournament } from '../models/ITournament'
+import { IEvent } from '../models/IEvent'
+
+import createExpandsString = Common.createExpandsString
+import TournamentOptions = ITournament.Options
+import EventOptions = IEvent.Options
+
+
 const TOURNAMENT_URL = 'https://api.smash.gg/tournament/%s?%s';
 const EVENT_URL = 'https://api.smash.gg/event/%s?%s';
 const PHASE_URL = 'https://api.smash.gg/phase/%s?%s';
 const PHASE_GROUP_URL = 'https://api.smash.gg/phase_group/%s?%s';
 
 
-export async function getTournamentData(tournamentId: string, expands: string): Promise<Entity>{
+export async function getTournamentData(tournamentId: string, options: TournamentOptions): Promise<Entity>{
     try{
-        let url: string = format(TOURNAMENT_URL, tournamentId);
+        let expands: string = createExpandsString(options.expands)
+        let url: string = format(TOURNAMENT_URL, tournamentId, expands);
         let data: Entity = JSON.parse(await request(url));
         return data;
     } catch(err){
@@ -24,9 +34,10 @@ export async function getTournamentData(tournamentId: string, expands: string): 
     }
 }
 
-export async function getEventData(eventId: number, expands: string): Promise<Entity>
+export async function getEventDataById(eventId: number, options: EventOptions): Promise<Entity>{
     try{
-        let url: string = format(EVENT_URL, eventId);
+        let expands: string = createExpandsString(options.expands)
+        let url: string = format(EVENT_URL, eventId, expands);
         let data: Entity = JSON.parse(await request(url));
         return data;
     } catch(err){

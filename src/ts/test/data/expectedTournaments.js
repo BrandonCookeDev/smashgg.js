762fd155a3958a162f4feeb4582ca333ef4d677f
Brandon Cooke <brandoncookedev@gmail.com>
2019-01-11 12:07:29 -0500

    Moving stuff around to make TS unit tests

---
+++ src/ts/test/data/expectedTournaments.js
@@ -0,0 +1,85 @@
+/* eslint-disable */
+
+module.exports = {
+	tournaments: {
+		function1: {
+			"entities": {
+				"tournament": {
+					"id": 7576,
+					"seriesId": null,
+					"ownerId": 421,
+					"state": 1,
+					"progressMeter": 0,
+					"name": "Function(1) - Recursion Regional",
+					"slug": "tournament/function-1-recursion-regional",
+					"shortSlug": "function1",
+					"venueFee": 20,
+					"processingFee": 5,
+					"processingBase": null,
+					"tiebreakOrder": null,
+					"timezone": "America/New_York",
+					"private": false,
+					"published": true,
+					"testMode": false,
+					"sandboxMode": false,
+					"publicSeeding": false,
+					"tournamentType": 1,
+					"hasOnlineEvents": false,
+					"includeQRCode": false,
+					"approved": true,
+					"startAt": 1491058800,
+					"endAt": 1491102000,
+					"startedAt": null,
+					"completedAt": null,
+					"registrationClosesAt": 1490853600,
+					"eventRegistrationClosesAt": 1490853600,
+					"teamCreationClosesAt": 1490932799,
+					"stripeMode": "live",
+					"paypalMode": null,
+					"paypalApp": "smashgg",
+				}
+			}
+		},
+		ceo2016: {
+			"entities": {
+				"tournament": {
+					"id": 1609,
+					"seriesId": null,
+					"ownerId": 3431,
+					"state": 1,
+					"progressMeter": 0,
+					"name": "CEO 2016",
+					"slug": "tournament/ceo-2016",
+					"shortSlug": "ceo",
+					"venueFee": null,
+					"processingFee": 5,
+					"processingBase": null,
+					"tiebreakOrder": null,
+					"timezone": "America/New_York",
+					"private": false,
+					"published": true,
+					"testMode": false,
+					"sandboxMode": false,
+					"publicSeeding": false,
+					"tournamentType": 1,
+					"hasOnlineEvents": false,
+					"includeQRCode": false,
+					"approved": true,
+					"startAt": 1466740800,
+					"endAt": 1467000000,
+					"startedAt": null,
+					"completedAt": null,
+					"registrationClosesAt": 1466683200,
+					"eventRegistrationClosesAt": 1465819200,
+					"teamCreationClosesAt": 1465905600,
+					"stripeMode": null,
+					"paypalMode": "live",
+					"paypalApp": "default",
+					"city": "Orlando",
+					"addrState": "FL",
+				}
+			}
+		},
+		to12: {}
+	}
+};

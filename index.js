4b2af2a5726b5e726a8b2ac9b469f5034202581c
Brandon Cooke <brandoncookedev@gmail.com>
2019-01-17 23:34:44 -0500

    Added better access to the log

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ exports.Tournament  = require('./src/js/lib/internal').Tournament;
 exports.PhaseGroup  = require('./src/js/lib/internal').PhaseGroup;
 exports.VideoGame	= require('./src/js/lib/internal').VideoGame;
 exports.Character 	= require('./src/js/lib/Character').Character;
+exports.Log         = require('./src/js/lib/util/Logger').default;
+exports.setLogLevel = require('./src/js/lib/util/Logger').setLogLevel;
+exports.addLog      = require('./src/js/lib/util/Logger').addLog;

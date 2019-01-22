4b1604d148e03142d62982efc1902a55a4b38b6f
Brandon Cooke <bcooke32@gatech.edu>
2019-01-21 20:27:09 -0500

    Added gulp build process to replace clunky npm scripts

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const path = require('path')
+const gulp = require('gulp')
+const ts = require('gulp-typescript')
+const mocha = require('gulp-mocha')
+
+const ROOT = __dirname
+const SRC_DIR = path.join(ROOT, 'src')
+const TS_DIR = path.join(SRC_DIR, 'ts')
+const JS_DIR = path.join(SRC_DIR, 'js')
+const DIST_DIR = path.join(ROOT, 'dist')
+const TEST_DIR = path.join(JS_DIR, 'test')
+
+function test(){
+	return gulp.src(TEST_DIR)
+		.pipe(mocha())
+}
+
+function tsc(){
+	return gulp.src(TS_DIR)
+		.pipe(ts({
+			rootDir: TS_DIR,
+			outDir: JS_DIR,
+			target: 'ES5',
+			module: 'commonjs',
+			lib: [
+				'dom',
+				'es2017'
+			],  
+			strict: true,
+			esModuleInterop: true
+		}))
+}
+
+function createDTs(){
+	return gulp.ssrc(TS_DIR)
+		.pipe(ts({
+			rootDir: TS_DIR,
+			target: 'ES5',
+			outFile: 'smashgg.js',
+			module: 'amd',
+			lib: [
+				'dom',
+				'es2017'
+			],  
+			declaration: true,
+			strict: true,
+			esModuleInterop: true
+		}))
+}
+
+function testTournament(){
+	return gulp.src(TEST_DIR, 'testTournament.js')
+		.pipe(mocha())
+}
+
+function testEvent(){
+	return gulp.src(TEST_DIR, 'testEvent.js')
+		.pipe(mocha())
+}
+
+function testPhase(){
+	return gulp.src(TEST_DIR, 'testPhase.js')
+		.pipe(mocha())
+}
+
+function testPhaseGroup(){
+	return gulp.src(TEST_DIR, 'testPhaseGroup.js')
+		.pipe(mocha())
+}
+
+function testCache(){
+	return gulp.src(TEST_DIR, 'testZCache.js')
+		.pipe(mocha())
+}
+function testPlayer(){
+	return gulp.src(TEST_DIR, 'testPlayer.js')
+		.pipe(mocha())
+}
+function testSet(){
+	return gulp.src(TEST_DIR, 'testSet.js')
+		.pipe(mocha())
+}
+
+
+
+exports.test = test
+exports.testTournament = testTournament
+exports.testEvent = testEvent
+exports.testPhase = testPhase
+exports.testPhaseGroup = testPhaseGroup
+exports.testCache = testCache
+exports.testPlayer = testPlayer
+exports.testSet = testSet
+exports.tsc = tsc
+exports.createDTs = createDTs

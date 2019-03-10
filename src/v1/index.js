7a0a18a6d0915a412b19f67371b6359d39a875cc
Brandon Cooke <brandoncookedev@gmail.com>
2019-03-10 17:06:46 -0400

    fixed v1 index

--- src/v1/index.js
+++ src/v1/index.js
@@ -8,8 +8,8 @@ exports.VideoGame   = require('./js/lib/VideoGame');
 exports.Character   = require('./js/lib/Character');
 exports.Player      = require('./js/lib/Player');
 
-exports.Log         = require('./src/v4/js/lib/util/Logger').default
-exports.setLogLevel = require('./src/v4/js/lib/util/Logger').setLogLevel
-exports.addLog      = require('./src/v4/js/lib/util/Logger').addLog
-exports.disableLog  = require('./src/v4/js/lib/util/Logger').disableLog
-exports.enableLog	= require('./src/v4/js/lib/util/Logger').enableLog
+exports.Log         = require('./js/lib/util/Logger').default
+exports.setLogLevel = require('./js/lib/util/Logger').setLogLevel
+exports.addLog      = require('./js/lib/util/Logger').addLog
+exports.disableLog  = require('./js/lib/util/Logger').disableLog
+exports.enableLog	= require('./js/lib/util/Logger').enableLog

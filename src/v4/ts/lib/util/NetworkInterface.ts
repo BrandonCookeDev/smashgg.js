7abd9c7eece21cf96ebc25063a17e86c1e25bbea
unknown <BrandonCookeDev@gmail.com>
2019-06-02 17:40:36 -0400

    modify query logs to stringify json

--- src/v4/ts/lib/util/NetworkInterface.ts
+++ src/v4/ts/lib/util/NetworkInterface.ts
@@ -54,7 +54,7 @@ export default class NetworkInterface{
 	 */
 	static query(query: string, variables: Variables) : Promise<any>{
 		return new Promise(function(resolve, reject){
-			log.queries("Query: " + query + ":\n" + variables);
+			log.queries("Query: " + JSON.stringify(query) + ":\n" + JSON.stringify(variables));
 			QueryQueue.getInstance().add(() => {
 				return NetworkInterface.client.request(query, variables)
 						.then(resolve)
@@ -64,8 +64,8 @@ export default class NetworkInterface{
 	}
 
 	static rawQuery(query: string, variables: Variables) : Promise<any>{
-		log.queries("Raw Query: " + query + ":\n" + variables);
 		return new Promise(function(resolve, reject){
+			log.queries("Raw Query: " + JSON.stringify(query) + ":\n" + JSON.stringify(variables));
 			QueryQueue.getInstance().add(() => {
 				return NetworkInterface.client.rawRequest(query, variables)
 					.then(resolve)
@@ -99,7 +99,7 @@ export default class NetworkInterface{
 
 	static async paginatedQuery(operationName: string, queryString: string, params: object, options?: IPaginatedQuery.Options, additionalParams?: {}, complexitySubtraction: number = 0) : Promise<any[]>{
 		log.info('%s: Calling Paginated Querys', operationName);
-		log.queries("Paginated Query: " + queryString + ":\n" + params);
+		log.queries("Paginated Query: " + JSON.stringify(queryString) + ":\n" + JSON.stringify(params));
 
 		let results = [];
 
